9e453b7e4fa31451c2f209c1c650a64752293a26
Ismael Narváez <ismnarber@outlook.es>
2020-09-20 14:01:33 +0200

    feat: add exportable classes

---
+++ src/json_api_validation/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.8
+
+# Copyright: Ismael Narváez Berenjeno
+
+from json_api_validation.document.data_document import DataDocument
+from json_api_validation.document.error_document import ErrorDocument
+
+__all__ = ["DataDocument", "ErrorDocument"]
