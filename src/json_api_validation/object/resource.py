dcc9faaaa0fd4d846599438bedd8b4c3b353cf31
Ismael Narváez <ismnarber@outlook.es>
2020-09-20 14:00:08 +0200

    fix: set Optional fields of Resource

--- src/json_api_validation/object/resource.py
+++ src/json_api_validation/object/resource.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3.8
 
 # Copyright: Ismael Narváez Berenjeno
+from typing import Optional
 
 from json_api_validation.object.attribute import Attribute
 from json_api_validation.object.meta import Meta
 from json_api_validation.object.relationship import Relantionship
-from json_api_validation.object.resource_identifier import (
-    ResourceIdentifier,
-)
+from json_api_validation.object.resource_identifier import ResourceIdentifier
 from json_api_validation.object.resource_link import ResourceLink
 
 
 class Resource(ResourceIdentifier):
-    attributes: Attribute
-    relationships: Relantionship
-    links: ResourceLink
-    meta: Meta
+    attributes: Optional[Attribute]
+    relationships: Optional[Relantionship]
+    links: Optional[ResourceLink]
+    meta: Optional[Meta]
