d4c31268a96fccfe5e4f5ee34e3e8e8dee5b6457
Ismael Narváez <ismnarber@outlook.es>
2020-09-25 23:10:01 +0200

    feat(data model): add included section model

--- src/json_api_validation/document/data_document.py
+++ src/json_api_validation/document/data_document.py
@@ -4,21 +4,29 @@
 
 from typing import List, Optional, Union
 
-from pydantic import create_model
-from pydantic.main import ModelMetaclass
-
 from json_api_validation.object.jsonapi import JSONAPI
 from json_api_validation.object.link import Link
 from json_api_validation.object.meta import Meta
 from json_api_validation.object.resource import Resource
+from pydantic import create_model
+from pydantic.main import ModelMetaclass
 
 
-def create_data_model(attribute: ModelMetaclass) -> ModelMetaclass:
+def create_data_model(
+    data_attributes_model: ModelMetaclass,
+    included_attributes_model: ModelMetaclass,
+) -> ModelMetaclass:
     return create_model(
         "DataDocument",
-        data=(Union[Resource[attribute], List[Resource[attribute]]], ...),
+        data=(
+            Union[
+                Resource[data_attributes_model],
+                List[Resource[data_attributes_model]],
+            ],
+            ...,
+        ),
         meta=(Optional[Meta], None),
         jsonapi=(Optional[JSONAPI], None),
         links=(Optional[Link], None),
-        included=(Optional[List[Resource[attribute]]], None),
+        included=(Optional[List[Resource[included_attributes_model]]], None),
     )
--- src/json_api_validation/document/error_document.py
+++ src/json_api_validation/document/error_document.py
@@ -4,22 +4,23 @@
 
 from typing import List, Optional
 
-from pydantic import create_model
-from pydantic.main import ModelMetaclass
-
 from json_api_validation.object.error import Error
 from json_api_validation.object.jsonapi import JSONAPI
 from json_api_validation.object.link import Link
 from json_api_validation.object.meta import Meta
 from json_api_validation.object.resource import Resource
+from pydantic import create_model
+from pydantic.main import ModelMetaclass
 
 
-def create_error_model(attribute: ModelMetaclass) -> ModelMetaclass:
+def create_error_model(
+    included_attributes_model: ModelMetaclass,
+) -> ModelMetaclass:
     return create_model(
         "ErrorDocument",
         errors=(List[Error], ...),
         meta=(Optional[Meta], None),
         jsonapi=(Optional[JSONAPI], None),
         links=(Optional[Link], None),
-        included=(Optional[List[Resource[attribute]]], None),
+        included=(Optional[List[Resource[included_attributes_model]]], None),
     )
