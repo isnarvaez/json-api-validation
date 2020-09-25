3324a583650a3c4d9cd59f3e62edd8a4239a53a7
Ismael Narváez <ismnarber@outlook.es>
2020-09-25 23:10:54 +0200

    feat: export general model creation functions

--- src/json_api_validation/__init__.py
+++ src/json_api_validation/__init__.py
@@ -2,7 +2,7 @@
 
 # Copyright: Ismael Narváez Berenjeno
 
-from json_api_validation.document.data_document import DataDocument
-from json_api_validation.document.error_document import ErrorDocument
+from json_api_validation.document.data_document import create_data_model
+from json_api_validation.document.error_document import create_error_model
 
-__all__ = ["DataDocument", "ErrorDocument"]
+__all__ = ["create_data_model", "create_error_model"]
