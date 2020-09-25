f3f358bfe13d209e7e5fe958df9c26971b310302
Ismael Narváez <ismnarber@outlook.es>
2020-09-25 23:27:10 +0200

    fix(error model): set primary fields correctly

--- src/json_api_validation/document/error_document.py
+++ src/json_api_validation/document/error_document.py
@@ -6,21 +6,15 @@ from typing import List, Optional
 
 from json_api_validation.object.error import Error
 from json_api_validation.object.jsonapi import JSONAPI
-from json_api_validation.object.link import Link
 from json_api_validation.object.meta import Meta
-from json_api_validation.object.resource import Resource
 from pydantic import create_model
 from pydantic.main import ModelMetaclass
 
 
-def create_error_model(
-    included_attributes_model: ModelMetaclass,
-) -> ModelMetaclass:
+def create_error_model() -> ModelMetaclass:
     return create_model(
         "ErrorDocument",
         errors=(List[Error], ...),
         meta=(Optional[Meta], None),
         jsonapi=(Optional[JSONAPI], None),
-        links=(Optional[Link], None),
-        included=(Optional[List[Resource[included_attributes_model]]], None),
     )
